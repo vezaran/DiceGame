8cee60b65c8e3f275be6b855a23922e8efc69c7f
epsilon <32664441+vezaran@users.noreply.github.com>
2017-10-10 02:21:57 -0400

    Update DiceGame.java

--- DiceGame.java
+++ DiceGame.java
@@ -2,14 +2,6 @@ import javax.swing.JOptionPane;
 import java.lang.Math;
 import java.util.Scanner;
 
-// Rules:
-// 2 dice
-// Roll both dice
-// 7 doubles player money
-// 11 breaks even
-// 2 busted
-// Any other number you lose your bet
-
 public class DiceGame {
   
   static int bank = 500;
